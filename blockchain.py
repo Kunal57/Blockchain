8580306ba38fc91fbf0a802e41815e7aa3385031
Kunal Patel <kunalpatelx92@gmail.com>
2017-11-16 03:53:12 -0600

    Add method for Registering Nodes

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from textwrap import dedent
+from urllib.parse import urlparse
 from time import time
 from uuid import uuid4
 
@@ -11,6 +12,7 @@ class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
+    self.nodes = set()
 
     # Create the genesis block
     self.new_block(previous_hash=1, proof=100)
@@ -56,6 +58,17 @@ class Blockchain(object):
       })
 
     return self.last_block['index'] + 1
+
+  def register_node(self, address):
+    '''
+    Add a new node to the list of nodes
+
+    :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+    :return: None
+    '''
+
+    parsed_url = urlparse(address)
+    self.nodes.add(parsed_url.netloc)
 
   @property
   def last_block(self):
